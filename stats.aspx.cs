f804c3e1d9f9ff9e49c9d3255116add4749682c6
Abdullah-sajid-003 <abdullahsajid1273@gmail.com>
2025-06-01 22:32:14 +0500

    stats.aspx.cs

---
+++ stats.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FeedbackSystem
+{
+    public partial class Stats : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int total = Application["TotalSubmissions"] != null ? (int)Application["TotalSubmissions"] : 0;
+            lblTotal.Text = "Total Feedback Submissions: " + total;
+
+            if (Session["LastStudentName"] != null)
+            {
+                lblLastUser.Text = "Your last submitted name: " + Session["LastStudentName"].ToString();
+            }
+        }
+    }
+}
