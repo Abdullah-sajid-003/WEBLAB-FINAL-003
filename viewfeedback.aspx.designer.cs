5ad0be1e5e206d36b0351450685ad82aa14f5b43
Abdullah-sajid-003 <abdullahsajid1273@gmail.com>
2025-06-01 22:40:19 +0500

    viewfeedback.aspx.designer.cs

---
+++ viewfeedback.aspx.designer.cs
@@ -0,0 +1,35 @@
+namespace FeedbackSystem
+{
+
+
+    public partial class ViewFeedback
+    {
+
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// GridView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+
+        /// <summary>
+        /// lnkBack control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink lnkBack;
+    }
+}
